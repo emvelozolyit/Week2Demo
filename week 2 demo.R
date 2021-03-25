6464fbc90c1df330f419712d5b478d9a52902338
Emily Velozo <L00162302@student.lyit.ie>
2021-03-24 20:08:15 -0400

    Added question 2 answers

--- week 2 demo.R
+++ week 2 demo.R
@@ -4,3 +4,17 @@ x
 # Assign number 5 to variable y
 y <- 5
 y
+# Check class of x
+class(x)
+# Check if x is numeric 
+is.numeric(x)
+#set 5 to an integer
+i <- 5L
+i
+is.integer(i)
+
+
+date1 <- as.Date("2019-03-08")
+date1
+class(date1)
+as.numeric(date1)
