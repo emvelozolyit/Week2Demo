2ee0019530f48854b65818be663bfae429c650f9
Emily Velozo <L00162302@student.lyit.ie>
2021-03-24 20:18:09 -0400

    basic building blocks lesson - number 1

--- week 2 demo.R
+++ week 2 demo.R
@@ -18,3 +18,33 @@ date1 <- as.Date("2019-03-08")
 date1
 class(date1)
 as.numeric(date1)
+
+# Construct a vector from a series of individual elements, suing the c() function
+c(10, 150, 30, 40, 55.6)
+
+
+#Vector Arithmetic 
+p <- c(1, 2, 4, 7)
+q <- c(5, 5, 5)
+r <- p + q
+
+
+install.packages("swirl")
+library(swirl)
+swirl()
+Emily
+3
+1
+1
+1
+5+7
+x <- 5+7
+x
+y <- x-3
+y
+c(1.1,9,3.14)
+z <- c(1.1,9,3.14)
+bye
+bye()
+swilr()
+swirl()
