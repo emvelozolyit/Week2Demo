ca3dbcdb0b9dba0d19f8936f9c3a2a27096f80e7
Emily Velozo <L00162302@student.lyit.ie>
2021-03-24 20:39:46 -0400

    vectors lesson

--- week 2 demo.R
+++ week 2 demo.R
@@ -48,3 +48,63 @@ bye
 bye()
 swilr()
 swirl()
+
+
+# Vector Arithmetic
+x <- c(10.4,5.6,3.1,6.4,21.7)
+y <- c(x, 0, x)
+z<- 2*x + y + 1
+z
+min(z)
+max(z)
+range(z)
+prod(z)
+mean(z)
+sorted_z <- sort(z)
+sorted_z
+
+
+#seq() function example
+seq3 <- seq(-100, 100, by=0.6)
+seq3
+mean((seq3))
+# sort seq in descending order
+sort(seq3, decreasing = TRUE)
+
+
+#Logical vectors
+z <- c(1:3, NA); result <- is.na(z)
+z
+
+#Interactive Swirl Test
+library(swirl)
+swirl()
+Emily
+2
+2
+2
+1
+4
+num_vect <- c(0.5, 55, -10, 6)
+tf <- num_vect < 1
+2
+tf
+num_vect >= 6
+2
+2
+1
+1
+2
+my_char <- c("My", "name", "is")
+my_char
+paste(my_char, collapse = " ")
+my_name <- c(my_char, "Emily")
+my_name
+paste(my_name, collapse = " ")
+paste("Hello", "world!", sep = " ")
+paste(1:3, c("X", "Y", "Z"), sep = "")
+paste (LETTERS, 1:4, sep = "-")
+1
+2
+bye()
+0
